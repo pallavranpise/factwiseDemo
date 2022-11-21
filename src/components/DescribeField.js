d7d6abd6d03d4a64687fb45374aabe9871475522
pallav ranpise <pallav.ranpise@gmail.com>
2022-11-21 16:17:39 +0530

    added delete

--- src/components/DescribeField.js
+++ src/components/DescribeField.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 export default function DescribeField({children,title}) {
   const hovergrey = {display:'flex',flexDirection:'column'}
   return (
-    <div style={hovergrey} >
-      <span style={{margin:'5px 10px',}} className="grayout">{title}</span> <span className='border' style={{margin:'10px',lineHeight:'1.4rem'}}>{children}</span>
+    <div style={hovergrey}  >
+      <span className="describeFirst grayout">{title}</span> <span  style={{margin:'10px',lineHeight:'1.4rem'}}>{children}</span>
     </div>
   );
 }
